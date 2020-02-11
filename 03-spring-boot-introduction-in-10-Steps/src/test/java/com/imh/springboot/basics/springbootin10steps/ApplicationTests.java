7ecc2d77008175271109cabb8681a3f2c92979de
jloreg <jregueiro85@gmail.com>
2020-02-11 10:27:24 +0100

    3.Spring Boot in 10 Steps

---
+++ 03-spring-boot-introduction-in-10-Steps/src/test/java/com/imh/springboot/basics/springbootin10steps/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imh.springboot.basics.springbootin10steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
