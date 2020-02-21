7a87e6ded20784aca3601c055e55cb039520c642
jloreg <jregueiro85@gmail.com>
2020-02-21 08:47:01 +0100

    5.Spring JDBC and JPA

---
+++ 05-spring-jdbc-to-jpa/src/test/java/com/imh/spring/database/databasedemo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imh.spring.database.databasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
